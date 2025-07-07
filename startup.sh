f93a66017955e94f12f6fe851e6a430f55fc1f4f
Gurram Venkata Rao <37859333+venkataraogurram@users.noreply.github.com>
2025-07-07 15:48:09 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "=== STARTING DJANGO APP ==="
-exec gunicorn erichermanndotcom.wsgi:application --bind 0.0.0.0:8000
+python manage.py collectstatic && gunicorn --workers 2 erichermanndotcom.wsgi
