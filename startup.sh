2953512228ee129f63c90477e202bba2b79c15a7
Gurram Venkata Rao <37859333+venkataraogurram@users.noreply.github.com>
2025-07-16 16:39:28 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "=== STARTING DJANGO APP ==="
-exec source venv/bin/activate && /usr/local/bin/gunicorn erichermanndotcom.wsgi:application --bind=0.0.0.0:8000
+. venv/bin/activate
+exec /usr/local/bin/gunicorn erichermanndotcom.wsgi:application --bind=0.0.0.0:8000
