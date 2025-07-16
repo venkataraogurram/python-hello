28972db5c873d988895edf206b2d13d48c28edbf
Gurram Venkata Rao <37859333+venkataraogurram@users.noreply.github.com>
2025-07-16 18:18:17 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 echo "=== STARTING DJANGO APP ==="
 . venv/bin/activate
 which gunicorn
-exec /usr/local/bin/gunicorn erichermanndotcom.wsgi:application --bind=0.0.0.0:8000
+export PATH=$PATH:$(pwd)/venv/bin
+exec gunicorn erichermanndotcom.wsgi:application --bind=0.0.0.0:8000
